d71d74564cd74a122d92df1933b02193b2104db6
LukaBuljevac <buljevacluka@gmail.com>
2026-01-15 19:56:48 +0100

    Main file changes

--- L2Automation.Demo/Program.cs
+++ L2Automation.Demo/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using L2Automation.Demo.Services;
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<EventStore>();
+builder.Services.AddSingleton<MachineStateService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
